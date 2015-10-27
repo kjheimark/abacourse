2719dd0e2c6ba0d8ff3940b5085d311c02978982
Karl Johan Heimark <kjheimark@comoyo.com>
2015-10-27 17:47:07 +0100

    Fix stupid bug

--- bootstrap-docker-ubuntu.sh
+++ bootstrap-docker-ubuntu.sh
@@ -3,6 +3,6 @@ sudo apt-get install docker-engine
 sudo docker run hello-world
 
 #THIS IS SO UGLY!!!
-curl -L https://github.com/docker/compose/releases/download/VERSION_NUM/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.4.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose --version
